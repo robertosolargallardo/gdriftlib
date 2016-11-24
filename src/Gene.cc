ee056dd7b93bf0c4a6ea6803fc9cacc9eb0fdb90
rsolar <roberto.solar.gallardo@gmail.com>
2016-11-24 10:23:59 -0300

    gene problem solved

--- src/Gene.cc
+++ src/Gene.cc
@@ -32,7 +32,10 @@ void Gene::mutation_rate(const double &_mutation_rate){
 }
 Gene::~Gene(void){
    this->_reference->decrease();
-   if(!this->_reference->read_only())
+   if(!this->_reference->read_only()){
       delete this->_reference;
-   this->_reference=nullptr;
+		this->_reference=nullptr;
+	}
+	else if(!this->_reference->count())
+		this->_reference=nullptr;
 }
