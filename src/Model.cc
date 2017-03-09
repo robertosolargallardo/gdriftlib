5c7d4fee2d4027471e3f30d0e51364fcf50e61f9
vsepulve <vsepulve@dcc.uchile.cl>
2017-03-09 12:20:21 -0300

    Ajustes menores para un nuevo merge

--- src/Model.cc
+++ src/Model.cc
@@ -6,10 +6,8 @@ namespace model{
       uniform_int_distribution<> uniform(0U,_src->size()-1U);
       uniform_real_distribution<double> mutation_rate(0.0,1.0);
 
-//      Reference* reference=nullptr;
-//      Reference* validator=nullptr;
-      VirtualSequence* reference=nullptr;
-      VirtualSequence* validator=nullptr;
+      Reference* reference=nullptr;
+      Reference* validator=nullptr;
 
       Individual* individual=nullptr;
 
@@ -19,8 +17,7 @@ namespace model{
          for(uint32_t cid=0;cid<individual->n_chromosomes();cid++){
             for(uint32_t gid=0;gid<individual->chromosome(cid)[0]->n_genes();gid++){
                if(mutation_rate(rng)<=individual->chromosome(cid)[0]->gene(gid)->mutation_rate()){
-//                  reference=new Reference(*(individual->chromosome(cid)[0]->gene(gid)->reference()));
-                  reference=new VirtualSequence(*(individual->chromosome(cid)[0]->gene(gid)->reference()));
+                  reference=new Reference(*(individual->chromosome(cid)[0]->gene(gid)->reference()));
                   reference->mutate();
 						
 						validator=_pool->push(cid,gid,reference);
@@ -32,6 +29,48 @@ namespace model{
                }
                else
                   _dst->at(id)->chromosome(cid)[0]->gene(gid)->reference(individual->chromosome(cid)[0]->gene(gid)->reference());
+            }
+         }
+      }
+   }
+   template<>
+   void run<WRIGHTFISHER,DIPLOID>(Population* &_src,Population* &_dst,Pool* &_pool){
+      constexpr uint32_t PARENTS=2U;
+      uniform_int_distribution<> coin(0U,1U);
+      uniform_int_distribution<> uniform(0U,_src->size()-1U);
+      uniform_real_distribution<double> mutation_rate(0.0,1.0);
+
+      Reference* reference=nullptr;
+      Reference* validator=nullptr;
+
+      array<Individual*,PARENTS> individual={nullptr,nullptr};
+      array<Gene*,PARENTS> gene={nullptr,nullptr};
+
+      for(uint32_t id=0U;id<_dst->size();id++){
+         individual[0]=_src->at(uniform(rng));
+         individual[1]=_src->at(uniform(rng));
+
+         for(uint32_t cid=0;cid<individual[0]->n_chromosomes();cid++){
+            for(uint32_t gid=0;gid<individual[0]->chromosome(cid)[0]->n_genes();gid++){
+               gene[0]=individual[0]->chromosome(cid)[coin(rng)]->gene(gid);
+               gene[1]=individual[1]->chromosome(cid)[coin(rng)]->gene(gid);
+               
+               for(uint32_t i=0U;i<uint32_t(DIPLOID);i++){
+                  if(mutation_rate(rng)<=gene[i]->mutation_rate()){
+                     reference=new Reference(*(gene[i]->reference()));
+                     reference->mutate();
+						
+   						validator=_pool->push(cid,gid,reference);
+                     if(validator!=nullptr){
+                        delete reference;
+                        reference=validator;
+                     }
+                     _dst->at(id)->chromosome(cid)[i]->gene(gid)->reference(reference);
+
+                  }
+                  else
+                     _dst->at(id)->chromosome(cid)[i]->gene(gid)->reference(gene[i]->reference());
+               }
             }
          }
       }
