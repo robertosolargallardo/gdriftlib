c3a3965c99fa64fa0ca7dd0ee326af5637e62900
vsepulve <vsepulve@dcc.uchile.cl>
2017-09-25 13:08:20 -0300

    Variable mt19937 rng replaced from an extern reference, to an instance member.

--- include/ModelWF.h
+++ include/ModelWF.h
@@ -11,6 +11,7 @@ class ModelWF : public Model{
 private:
 	unsigned int processDNAGenes(Population *dst, Pool *pool, Individual::Profile *profile, unsigned int plo, unsigned int chrid, unsigned int genid);
 //	unsigned int processMSGenes(Population *dst, Pool *pool, Individual::Profile *profile, unsigned int plo, unsigned int chrid, unsigned int genid);
+	mt19937 *rng;
 	
 public:	
 	ModelWF();
