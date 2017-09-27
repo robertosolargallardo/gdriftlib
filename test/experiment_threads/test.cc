4fb457902d9ba2f8de1c1d4ca389355626635651
vsepulve <vsepulve@dcc.uchile.cl>
2017-09-27 14:41:49 -0300

    New version of the threads testing program, DummyThread activated to verify first.

--- test/experiment_threads/test.cc
+++ test/experiment_threads/test.cc
@@ -10,8 +10,6 @@
 using namespace std;
 using boost::property_tree::ptree;
 
-// Soporte para extern rng
-// Esto debe ser reemplazado por una instancia de mt19937 en el thread
 random_device seed;
 mt19937 rng(seed());
 
@@ -171,13 +169,14 @@ void SimultionThread(unsigned int pid, unsigned int n_threads){
 	
 	NanoTimer timer;
 	unsigned int procesados = 0;
-//	unsigned int cur_pos = 0;
-	unsigned int cur_pos = pid;
+	unsigned int cur_pos = 0;
+	
+	double model_time = 0;
 	
 	while(true){
-//		global_mutex.lock();
-//		cur_pos = global_pos++;
-//		global_mutex.unlock();
+		global_mutex.lock();
+		cur_pos = global_pos++;
+		global_mutex.unlock();
 		
 		if( cur_pos >= work_queue.size() ){
 			cout<<"SimultionThread["<<pid<<"] - Cola de trabajo vacia, saliendo\n";
@@ -191,35 +190,135 @@ void SimultionThread(unsigned int pid, unsigned int n_threads){
 		Simulator sim(fjob);
 //		cout<<"SimultionThread["<<pid<<"] - Simulator::run\n";
 		sim.run();
-		
-		// Estadisticos e indices
-//		cout<<"SimultionThread["<<pid<<"] - Calculando estdisticos\n";
-		Sample all("summary");
-		map<string, Sample*> samples = sim.samples();
-		for(map<string, Sample*>::iterator i = samples.begin(); i != samples.end(); ++i){
-			ptree findices = i->second->indices();
-			
-//			stringstream ss;
-//			write_json(ss, findices);
-//		
-//			global_mutex.lock();
-//			cout << ss.str() << endl;
-//			global_mutex.unlock();
-		
-			all.merge(i->second);
-		}
-		ptree findices = all.indices();
+		model_time += sim.model_time;
 		
 		// Parte local del analyzer
 		// Esto requiere el target 
 		// Falta definir e implementar la normalizacion
 		
-		
-		cur_pos += n_threads;
 	}
 	
 //	global_mutex.lock();
-	cout<<"SimultionThread["<<pid<<"] - Fin (Total trabajos: "<<procesados<<", Total ms: "<<timer.getMilisec()<<")\n";
+	cout<<"SimultionThread["<<pid<<"] - Fin (Total trabajos: "<<procesados<<", Total ms: "<<timer.getMilisec()<<", Model ms: "<<model_time<<")\n";
+//	global_mutex.unlock();
+	
+}
+
+void SimultionThreadLocalParsing(unsigned int pid, unsigned int local_jobs, ptree fsettings){
+	
+//	global_mutex.lock();
+	cout<<"SimultionThreadLocalParsing["<<pid<<"] - Inicio\n";
+//	global_mutex.unlock();
+	
+	NanoTimer timer;
+	unsigned int procesados = 0;
+	
+	string sim_id = fsettings.get<string>("id");
+	vector<ptree> scenarios;
+	for(auto &fscenario : fsettings.get_child("scenarios")){
+		scenarios.push_back(fscenario.second);
+	}
+	
+	double model_time = 0;
+	
+	for(unsigned int i = 0; i < local_jobs; ++i){
+		
+		ptree fjob;
+		fjob.put("id", sim_id);
+		fjob.put("run", i);
+		fjob.put("batch", 0);
+		fjob.put("feedback", 0);
+		fjob.put("batch-size", local_jobs);
+		fjob.put("max-number-of-simulations", local_jobs);
+		fjob.add_child("individual", parse_individual(fsettings.get_child("individual")));
+		fjob.add_child("scenario", parse_scenario(scenarios[i%scenarios.size()], 100));
+		
+		++procesados;
+		
+//		cout<<"SimultionThreadLocalParsing["<<pid<<"] - Creando Simulator para tarea "<<i<<"\n";
+		Simulator sim(fjob);
+//		cout<<"SimultionThreadLocalParsing["<<pid<<"] - Simulator::run\n";
+		sim.run();
+		model_time += sim.model_time;
+		
+		// Parte local del analyzer
+		// Esto requiere el target 
+		// Falta definir e implementar la normalizacion
+		
+	}
+	
+//	global_mutex.lock();
+	cout<<"SimultionThreadLocalParsing["<<pid<<"] - Fin (Total trabajos: "<<procesados<<", Total ms: "<<timer.getMilisec()<<", Model ms: "<<model_time<<")\n";
+//	global_mutex.unlock();
+	
+}
+
+void DummyThreadLocalParsing(unsigned int pid, unsigned int local_jobs, ptree fsettings){
+	
+//	global_mutex.lock();
+	cout<<"DummyThreadLocalParsing["<<pid<<"] - Inicio\n";
+//	global_mutex.unlock();
+	
+	NanoTimer timer;
+	unsigned int procesados = 0;
+	
+	string sim_id = fsettings.get<string>("id");
+	vector<ptree> scenarios;
+	for(auto &fscenario : fsettings.get_child("scenarios")){
+		scenarios.push_back(fscenario.second);
+	}
+	
+	double model_time = 0;
+	
+	for(unsigned int i = 0; i < local_jobs; ++i){
+		
+		ptree fjob;
+		fjob.put("id", sim_id);
+		fjob.put("run", i);
+		fjob.put("batch", 0);
+		fjob.put("feedback", 0);
+		fjob.put("batch-size", local_jobs);
+		fjob.put("max-number-of-simulations", local_jobs);
+		fjob.add_child("individual", parse_individual(fsettings.get_child("individual")));
+		fjob.add_child("scenario", parse_scenario(scenarios[i%scenarios.size()], 100));
+		
+		++procesados;
+		
+//		cout<<"DummyThreadLocalParsing["<<pid<<"] - Creando Simulator para tarea "<<i<<"\n";
+		
+		NanoTimer sim_timer;
+		unsigned int n_gens = 10000;
+		unsigned int pop_size = 100000;
+		unsigned int *src = new unsigned int[pop_size];
+		unsigned int *dst = new unsigned int[pop_size];
+		for(unsigned int k = 0; k < pop_size; ++k){
+			src[k] = k;
+		}
+		for(unsigned int g = 0; g < n_gens; ++g){
+			for(unsigned int k = 0; k < pop_size; ++k){
+				if( (k & 0x1) == 0 ){
+					dst[i] = src[ ((k * g)>>2) % pop_size ];
+				}
+				else{
+					dst[i] = src[ ((k * g)<<2) % pop_size ];
+				}
+			}
+			unsigned int *tmp = src;
+			src = dst;
+			dst = tmp;
+		}
+		model_time += sim_timer.getMilisec();
+		
+		
+		
+		// Parte local del analyzer
+		// Esto requiere el target 
+		// Falta definir e implementar la normalizacion
+		
+	}
+	
+//	global_mutex.lock();
+	cout<<"DummyThreadLocalParsing["<<pid<<"] - Fin (Total trabajos: "<<procesados<<", Total ms: "<<timer.getMilisec()<<", Model ms: "<<model_time<<")\n";
 //	global_mutex.unlock();
 	
 }
@@ -243,31 +342,24 @@ int main(int argc,char** argv){
 	cout<<"Test - Preparando Cola de Trabajo\n";
 	fill_queue(fsettings, total);
 	
-	// Solo para probar
-//	cout<<"Test - Resultados:\n";
-//	for(unsigned int i = 0; i < work_queue.size(); ++i){
-//		cout<<"----- Trabajo "<<i<<" ------\n";
-//		std::stringstream ss;
-//		write_json(ss, work_queue[i]);
-//		cout<<ss.str()<<"\n";
-//		cout<<"-----      ------\n";
-//	}
 	double ms_preparation = timer.getMilisec();
 	cout<<"Test - Preparacion terminada en "<<ms_preparation<<" ms\n";
 	
 	cout<<"Test - Iniciando "<<n_threads<<" threads\n";
 	vector<thread> threads_list;
 	for(unsigned int i = 0; i < n_threads; ++i){
-		threads_list.push_back( thread(SimultionThread, i, n_threads) );
+//		threads_list.push_back( thread(SimultionThread, i, n_threads) );
+//		threads_list.push_back( thread(SimultionThreadLocalParsing, i, total/n_threads, fsettings) );
+		threads_list.push_back( thread(DummyThreadLocalParsing, i, total/n_threads, fsettings) );
 	
-		// Tomar pthread de este thread
-		pthread_t current_thread = threads_list.back().native_handle();
-		// Preparar datos para setear afinidad
-		cpu_set_t cpuset;
-		CPU_ZERO(&cpuset);
-		CPU_SET(i, &cpuset);
-		// Setear afinidad
-		pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
+//		// Tomar pthread de este thread
+//		pthread_t current_thread = threads_list.back().native_handle();
+//		// Preparar datos para setear afinidad
+//		cpu_set_t cpuset;
+//		CPU_ZERO(&cpuset);
+//		CPU_SET(i, &cpuset);
+//		// Setear afinidad
+//		pthread_setaffinity_np(current_thread, sizeof(cpu_set_t), &cpuset);
 		
 	}
 	for(unsigned int i = 0; i < n_threads; ++i){
